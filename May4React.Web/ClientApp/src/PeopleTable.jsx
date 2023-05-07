329630271f8e2a272fe6a646e5bfb9fc625ebf61
Sora Bloch <sorahindybloch@gmail.com>
2023-05-07 13:47:28 -0400

    Add project files.

---
+++ May4React.Web/ClientApp/src/PeopleTable.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PersonRow from './PersonRow';
+import PersonForm from './PersonForm';
+
+class PeopleTable extends React.Component {
+    state = {
+        firstName: '',
+        lastName: '',
+        age: '',
+        people: []
+    }
+
+    onFirstNameTextboxChange = (e) => {
+        this.setState({ firstName: e.target.value } );
+
+    }
+    onLastNameTextboxChange = (e) => {
+        this.setState({ lastName: e.target.value } );
+    }
+    onAgeTextboxChange = (e) => {
+        this.setState( { age: e.target.value });
+    }
+
+   
+    onAddClick = () => {
+        const copy = [...this.state.people];
+        copy.push({ firstName: this.state.firstName, lastName: this.state.lastName, age: this.state.age });
+        this.setState({ people: copy, firstName: '', lastName: '', age: ''});
+    }
+
+   
+
+
+    render() {
+        return (
+            <>
+                <PersonForm firstName={this.state.firstName}
+                    lastName={this.state.lastName}
+                    age={this.state.age }
+                    onFirstNameTextboxChange={this.onFirstNameTextboxChange}
+                    onLastNameTextboxChange={this.onLastNameTextboxChange}
+                    onAgeTextboxChange={this.onAgeTextboxChange}
+                    onAddClick={this.onAddClick}/>
+                <div className="container mt-5">
+                    <div className="table-responsive">
+                        <table className="table table-bordered table-striped table-hover">
+                            <thead className="table-dark">
+                                <tr>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Age</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <PersonRow people={this.state.people} />
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </>
+        );
+    }
+};
+
+export default PeopleTable;
+
+
+
+
---
+++ May4React.Web/ClientApp/src/PersonForm.jsx
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+
+class PersonForm extends React.Component {
+
+    render() {
+        return (
+            <div className='row'>
+                <div className='col-md-6'>
+                    <input value={this.props.firstName} onChange={this.props.onFirstNameTextboxChange} type='text' placeholder='First name' className='form-control' />
+                </div>
+                <div className='col-md-6'>
+                    <input value={this.props.lastName} onChange={this.props.onLastNameTextboxChange} type='text' placeholder='Last name' className='form-control' />
+                </div>
+                <div className='col-md-6'>
+                    <input value={this.props.age} onChange={this.props.onAgeTextboxChange} type='text' placeholder='Age' className='form-control' />
+                </div>
+                <div className='col-md-1'>
+                    <button onClick={this.props.onAddClick} className='btn btn-dark w-100'>Add</button>
+
+                </div>
+            </div>
+        );
+    }
+};
+
+export default PersonForm;
